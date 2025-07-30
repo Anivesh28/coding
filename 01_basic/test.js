8f43a133f40ca683ac0cab4b6e95288739bea733
Anivesh soni <aniveshsoni28@gmail.com>
2025-07-30 21:08:10 +0000

    third commit

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,21 @@
-console.log("hitesh")
+let hello = "5ac"
+console.log(typeof hello );
+let xxx= Number(hello)
+console.log(typeof xxx);
+console.log(xxx);
+//"33"=>33
+//"33abc"=>NaN
+//true=>1  or false=>0
+//These are the conversions or type converions
+let xyx="anivesh"
+let x=Boolean(xyx)
+console.log( x);
+//which will rewsult in true 
+//1=> true in boolean converion
+//0=> false in boolean converion
+//""=>false
+//"anivesh"=>true
+//********operations********** */
+console.log("2"+2+2) //results in 222
+console.log(2+2+"2") //results in 42
+console.log(2+"2"+2) //results in 222
